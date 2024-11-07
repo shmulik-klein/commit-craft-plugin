c5022e8611f7aed10387a7b27b2f26c7c8c868f1
shmulik-klein <shmulik.klein@gmail.com>
2024-11-07 20:31:18 +0100

    doc: add a stack overflow search action documentation

--- src/main/kotlin/com/github/shmulikklein/commitcraft/actions/SearchSoAction.kt
+++ src/main/kotlin/com/github/shmulikklein/commitcraft/actions/SearchSoAction.kt
@@ -4,6 +4,9 @@ import com.intellij.ide.BrowserUtil
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
+/**
+ * Provides an action that opens the web browser and navigates to the "Ask Question" page on Stack Overflow when triggered.
+ */
 class SearchSoAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         BrowserUtil.browse("https://stackoverflow.com/questions/ask")
